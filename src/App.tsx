7f5d6cfcb92daa8d4a0bd4ba40971352f5e7b4dc
Kacper Olek <kacper.olek@gmail.com>
2024-11-12 19:50:58 +0100

    App skeleton added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import "./App.css";
+import {Dashboard} from "./Dashboard/Dashboard.tsx";
+import styles from "./App.module.css";
 
 export function App() {
-  return <></>;
+  return <div className={styles.app}><Dashboard/></div>;
 }
---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,6 @@
+import styles from "./Dashboard.module.css"
+import {Button} from "../Button";
+
+export function Dashboard() {
+    return <div className={styles.dashboard}><Button/></div>
+}
